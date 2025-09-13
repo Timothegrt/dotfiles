2091f4024a20ce556aee209b5b4fa8968c91ddb5
timkicker <tim@kicker.dev>
2025-09-13 17:43:10 +0200

    Add bin scripts to dotfiles (adopt)

--- bin/.local/bin/cliphist-watch.sh
+++ bin/.local/bin/cliphist-watch.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 # Default-Clipboard
 wl-paste --type text  --watch cliphist store &
 wl-paste --type image --watch cliphist store &
-# Primary-Selection (optional, einige Apps nutzen die)
+# Primary-Selection 
 wl-paste --primary --type text  --watch cliphist store &
 wl-paste --primary --type image --watch cliphist store &
 wait
