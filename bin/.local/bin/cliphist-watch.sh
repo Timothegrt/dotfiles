6b918aa592402cf7ae34550d5bbe4f0d8628cb62
Timothegrt <125091658+Timothegrt@users.noreply.github.com>
2025-11-04 15:04:28 +0100

    Delete bin/.local/bin directory

--- bin/.local/bin/cliphist-watch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# Default-Clipboard
-wl-paste --type text  --watch cliphist store &
-wl-paste --type image --watch cliphist store &
-# Primary-Selection 
-wl-paste --primary --type text  --watch cliphist store &
-wl-paste --primary --type image --watch cliphist store &
-wait
